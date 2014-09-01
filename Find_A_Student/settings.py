6f754746cc37d716ae96772b720f67878bf76ade
sadraredape <s.shokouhi71@gmail.com>
2014-09-01 15:01:22 +0800

    setting changes

--- Find_A_Student/settings.py
+++ Find_A_Student/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',   
     'django.contrib.staticfiles',
     'static_pages',
-    'django.contrib.admin',
     'south',
     'accounts',
     'employer',
